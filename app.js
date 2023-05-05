213e5358a5bc46972736270e46facd203ebf6599
Christian Alexander <christianalexander@christians-mbp.lan>
2023-05-05 09:14:27 -0700

    Player can now purchase and use scissors

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ let tools = [
     type: "Teeth",
     pay: 1,
   },
+  {
+    type: "rusty scissors",
+    pay: 5,
+  },
 ];
 
 // I set the money that the landscaper starts with
@@ -17,7 +21,6 @@ function cutWithTeeth() {
   if (money < 5) {
     money += 1;
     accountBal.splice(0, 1, money);
-    console.log(accountBal)
     alert(
       `You did it! You earned $${money} cutting lawns today! You got a jaw that won't quit! You now have $${money}`
     );
@@ -27,4 +30,21 @@ function cutWithTeeth() {
     );
   }
 }
-// cutWithTeeth();
+
+let newTool = "";
+function buyScissors() {
+  if (accountBal < 5) {
+    alert(
+      `Sorry you only have $${money}, that's not enough to purchase this item. Get back to using those ${tools[0].type}!`
+    );
+  } else if (accountBal >= 5) {
+    document.getElementById("scissors");
+    accountBal.splice(0, 1, (money -= 5));
+    newTool = tools[1].type;
+    alert(
+      `You got some ${tools[1].type}! You can now cut lawns for $${tools[1].pay}! Your dentist will thank you later!`
+    );
+  } else {
+    alert(`You need more money to buy this.. get back to chewing!`)
+  }
+}
